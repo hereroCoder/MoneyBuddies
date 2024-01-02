1befeb9f3f74fe54eab28d1442af051d808d652a
hereroCoder <83869107+hereroCoder@users.noreply.github.com>
2024-01-02 21:38:19 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function showMenu(){
+ document.querySelector('nav').style.right = '0';
+ document.querySelector('nav').style.boxShadow= "0 0 0 100vw rgba(0,0,0,0.8)";
+}
+
+function hideMenu(){
+ document.querySelector('nav').style.right = '-200px';
+ document.querySelector('nav').style.boxShadow = "none";
+}
+
+function account(){
+ document.querySelector('form').style.right = '0';
+ document.querySelector('form').style.boxShadow= "0 0 0 100vw rgba(0,0,0,0.8)";
+ 
+}
+
+
+ 
+ 
