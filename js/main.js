bb0638cd5ddc43e975a45548fc94e47f597c0c47
hereroCoder <83869107+hereroCoder@users.noreply.github.com>
2024-01-03 01:47:16 +0000

    footer done

--- js/main.js
+++ js/main.js
@@ -4,15 +4,37 @@ function showMenu(){
 }
 
 function hideMenu(){
- document.querySelector('nav').style.right = '-200px';
- document.querySelector('nav').style.boxShadow = "none";
+ document.querySelector('nav').style.right = '-200%';
+};
+
+let registerBtn = document.querySelector('.account-form .register-btn');
+let loginBtn = document.querySelector('.account-form .login-btn');
+
+//i love this
+registerBtn.onclick = ()=>{
+  registerBtn.classList.add('active')
+  loginBtn.classList.remove('active')
+  document.querySelector('.register-form').style.display ='block'
+  document.querySelector('.login-form').style.display ='none'
 }
 
-function account(){
- document.querySelector('form').style.right = '0';
- document.querySelector('form').style.boxShadow= "0 0 0 100vw rgba(0,0,0,0.8)";
- 
+loginBtn.onclick = ()=>{
+  registerBtn.classList.remove('active')
+  loginBtn.classList.add('active')
+  document.querySelector('.register-form').style.display ='none'
+  document.querySelector('.login-form').style.display ='block'
 }
+
+function closeAccountForm(){
+  document.querySelector('.account-form').style.right = '-200%'
+}
+
+function openAccountForm(){
+  document.querySelector('.account-form').style.right = '0';
+  document.querySelector('.account-form').style.boxShadow= "0 0 0 100vw rgba(0,0,0,0.8)";
+}
+
+
 
 
  
